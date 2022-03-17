784b2bd33cf92cd1c202cad556223cff7c3c6005
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-03-17 18:03:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var database;
+var form,
+  game,
+  friend,
+  playerCount = 0;
+ 
+var gameState = 0;
+var totalresponse=0;
+var title, view;
+var allinfo;
+var element;
+function preload() {
+  bgimg = loadImage("bgpic.png");
+}
+
+function setup() {
+ 
+  canvas = createCanvas(950,450);
+  database = firebase.database();
+  title = createElement('h1');
+  view = createButton("View");
+ }
+
+function draw() {
+  background(bgimg)
+//Getting total number of responses
+
+var countref = database.ref("friendCount");
+countref.on("value",(data)=>{
+    totalresponse = data.val();
+})
+
+title.html("Your "+ totalresponse + " friends have filled the Scrapbook");
+title.position(100,50);
+
+view.position(700,80);
+view.size(100,30);
+
+
+
+
+
+}
+
